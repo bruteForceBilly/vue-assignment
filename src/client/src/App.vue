01cb5656e4a909fc2fa124be3d09e4cfd277681f
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-06-30 18:58:25 +0200

    render out socket data in client app

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
-    <div>Hello World</div>
+    <div>Hello World {{ test }}</div>
   </div>
 </template>
 
 <script>
+const url = 'ws://localhost:3000'
+const connection = new WebSocket(url, 'echo-protocol')
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      test: 'init'
+    }
+  },
+  mounted() {
+    connection.onopen = () => {
+      console.log('WS connection is open')
+    }
+    connection.onmessage = (e) => {
+      this.test = e.data
+    }
+  }
 }
 </script>
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -2,16 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
 
-const url = 'ws://localhost:3000'
-const connection = new WebSocket(url, 'echo-protocol')
-
-connection.onopen = () => {
-  console.log('WS connection is open')
-}
-connection.onmessage = (e) => {
-  console.log(e.data)
-}
-
 Vue.config.productionTip = false
 
 new Vue({
